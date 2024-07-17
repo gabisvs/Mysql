c9e2d8630a39c5a77795934fe292dd2617d728d4
Gabrielli <gabivirgilio@oultook.com>
2024-07-17 12:12:12 -0300

    Aula 02

--- aula 02/quitanda.sql
+++ aula 02/quitanda.sql
@@ -15,64 +15,108 @@ PRIMARY KEY(id)
 
 DESCRIBE tb_produtos;
 
-INSERT INTO tb_produtos (nome, quantidade, datavalidade, preco) 
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco) 
 VALUES ("tomate", 100, "2024-12-16", 8.00); 
-INSERT INTO tb_produtos(nome, quantidade, datavalidade, preco) 
+
+INSERT INTO tb_produtos(nomeproduto, quantidade, datavalidade, preco) 
 VALUES ("maçã", 20, "2023-12-15", 5.00);
-INSERT INTO Tb_produtos(nOme, Quantidade, Datavalidade, Preco) 
+
+INSERT INTO tb_produtos(nomeproduto, Quantidade, Datavalidade, Preco) 
 VALUES ("Laranja", 50, "2023-12-15", 10.00);
-INSERT INTO Tb_produtos(nOme, Quantidade, Datavalidade, Preco) 
+
+INSERT INTO tb_produtos(nomeproduto, Quantidade, Datavalidade, Preco) 
 vaLUES ("Banana", 200, "2023-12-15", 12.00);
-INSERT INTO Tb_produtos(nOme, Quantidade, Datavalidade, Preco) 
+
+INSERT INTO tb_produtos(nomeproduto, Quantidade, Datavalidade, Preco) 
 VALUES ("Uva", 1200, "2023-12-15", 30.00);
-INSERT INTO Tb_produtos(nOme, Quantidade, Datavalidade, Preco) 
+
+INSERT INTO tb_produtos(nomeproduto, Quantidade, Datavalidade, Preco) 
 VALUES ("Pêra", 500, "2023-12-15", 2.99);
 
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Batata doce", 2000, "2022-03-09", 10.00, 3);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Alface", 300, "2022-03-10", 7.00, 2);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Cebola", 1020, "2022-03-08", 5.00, 3);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES("Ovo Branco", 1000, "2022-03-07", 15.00, 5);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES("Agrião", 1500, "2022-03-06", 3.00, 2);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES("Cenoura", 1800, "2022-03-09", 3.50, 3);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Pimenta", 1100, "2022-03-15", 10.00, 4);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Alecrim", 130, "2022-03-10", 5.00, 4);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Manga", 200, "2022-03-07", 5.49, 1);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Couve", 100, "2022-03-12", 1.50, 2);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES("Rabanete", 1200, "2022-03-15", 13.00, 3);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, datavalidade, preco, categoriaid)
+VALUES ("Grapefruit", 3000, "2022-03-13", 50.00, 1);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, preco)
+VALUES("Sacola Cinza", 1118, 0.50);
+
+INSERT INTO tb_produtos (nomeproduto, quantidade, preco)
+VALUES("Sacola Verde", 1118, 0.50);
 
 -- Visualizar todos os dados na Tabela
-selECT * fRoM TB_produtos; 
+SELECT * FROM tb_produtos; 
 
 -- Selecionar algum dado para visualizar na Tabela
-select nome, preco from tb_produtos; 
+SELECT nome, preco FROM tb_produtos; 
 
 -- Para buscar algum dado específico na Tabela 
-select * from tb_produtos where id = 2; 
+SELECT * FROM tb_produtos WHERE id = 2; 
 
 -- Para buscar algum dado com Operadores Relacionais na Tabela 
-select * from tb_produtos where preco > 5.00; 
+SELECT * FROM tb_produtos WHERE preco > 5.00; 
 
 -- Selecionando dados com os Operadores Lógicos na Tabela
-select * from tb_produtos where preco > 5 and quantidade < 100; 
+SELECT * FROM tb_produtos WHERE preco > 5 AND quantidade < 100; 
 
 -- ------------------------------------------------------------------------
 -- Para adicionar uma nova Tabela
-alter table tb_produtos add descricao varchar(255);
+ALTER TABLE tb_produtos ADD descricao VARCHAR(255);
 
 -- Para apagar uma Tabela 
-alter table tb_produtos drop descricao; 
+ALTER TABLE tb_produtos DROP descricao; 
 
 -- Para alterar o nome na Tabela
-alter table tb_produtos change nome nomeproduto varchar(255); 
+ALTER TABLE tb_produtos CHANGE nome nomeproduto VARCHAR(255); 
 
 -- ------------------------------------------------------------------------
 SET SQL_SAFE_UPDATE = 0;
 
 -- Sem o Where, iria atualizar todos os precos da Tabela
-update tb_produtos set  preco = 2.99 where id = 1;
+UPDATE tb_produtos SET  preco = 2.99 WHERE id = 1;
 
 -- Para deletar um id dentro da Tabela 
-delete from tb_ produtos where id = 1; 
+DELETE FROM tb_ produtos WHERE id = 18; 
 -- Inserindo IN depois do id e mais dados (1,3), será apagado os dois id na Tabela
--- delete from tb_ produtos where id in (1,3); 
+DELETE FROM tb_produtos WHERE id IN (18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28);
 
 -- -------------------------------------------------------------------------
 
 -- Para definir o valor decimal, sendo 6 casas antes da vírgula e 2 depois 
-alter table tb_produtos modify preco decimal(6,2); 
+ALTER TABLE tb_produtos MODIFY preco DECIMAL(6,2); 
 
 -- -------------------------------------------------------------------------
-
-
 
 
 
